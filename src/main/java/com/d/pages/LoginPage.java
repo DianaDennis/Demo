d81437dfe5792ddbfb2d77990cfd9350ae9d3d8b
Diana <kovas06@yahoo.com>
2021-10-24 13:23:14 -0400

    added page feture code

--- src/main/java/com/d/pages/LoginPage.java
+++ src/main/java/com/d/pages/LoginPage.java
@@ -2,4 +2,10 @@ package com.d.pages;
 
 public class LoginPage {
 
+	public void LoginPage() {
+		System.out.println("Login page");
+	}
+	public void LoginSet() {
+		System.out.println("Login set");
+	}
 }
