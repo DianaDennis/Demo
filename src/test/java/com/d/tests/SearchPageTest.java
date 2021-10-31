95f5a476da0627b912d9c7b24265d1aeb5a01505
DianaDennis <kovas06@yahoo.com>
2021-10-31 14:46:48 -0400

    files to be commited

---
+++ src/test/java/com/d/tests/SearchPageTest.java
@@ -0,0 +1,11 @@
+package com.d.tests;
+
+import org.testng.annotations.Test;
+
+public class SearchPageTest {
+	@Test
+	public void searchPageTest() {
+		System.out.println("Search Page Test");
+	}
+
+}
