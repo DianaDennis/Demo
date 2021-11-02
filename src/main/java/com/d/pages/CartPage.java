4e131100c734708bb8252839b6406f0a0f7ec16a
DianaDennis <kovas06@yahoo.com>
2021-11-02 19:44:46 -0400

    updated cart page

--- src/main/java/com/d/pages/CartPage.java
+++ src/main/java/com/d/pages/CartPage.java
@@ -2,4 +2,9 @@ package com.d.pages;
 
 public class CartPage {
 
+	public void cartPage() {
+		
+		System.out.println("This is my cart page on a branch");
+		System.out.println("cart page frature");
+	}
 }
